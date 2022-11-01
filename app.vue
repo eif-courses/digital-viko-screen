65619ba1e0fc35302414090f497aaac84373b9f0
eif-courses <m.gzegozevskis@gmail.com>
2022-11-01 22:22:38 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+
+
+
+  <div class="viko-style grid grid-nogutter surface-section text-800">
+    <div class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+      <section>
+        <span class="block text-6xl font-bold mb-1">Pasirinkite dominančią temą</span>
+        <br/>
+        <Button label="Kabinetų paieška" class="mr-3 p-button-raised p-button-rounded p-button-lg" />
+        <Button label="Aktuali informacija" class="p-button-raised p-button-rounded p-button-lg" />
+
+      </section>
+    </div>
+    <div class="col-12 md:col-6 overflow-hidden">
+      <img src="https://i.ibb.co/cCtZGyR/viko.jpg" alt="Image" class="md:ml-auto block md:h-full" style="clip-path: polygon(8% 0, 100% 0%, 100% 100%, 0 100%)">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useToast } from "primevue/usetoast";
+
+const text = ref();
+const toast = useToast();
+const greet = () => {
+  toast.add({ severity: "info", summary: "Hello " + text.value });
+};
+</script>
+
+<style>
+.viko-style {
+  background-image: linear-gradient(180deg, #2af598 0%, #009efd 100%);
+}
+</style>
